be4e452cab664880c08fd7d25344d144d692d0c2
Blair <criticalimpact@gmail.com>
2025-07-23 22:56:48 +1000

    yolo

--- anime_tools/download_database.py
+++ anime_tools/download_database.py
@@ -1,25 +1,29 @@
 import os
-from urllib.request import URLopener
+import time
+import urllib.request
+import shutil
 import time
 
 from cement import fs
+
+DB_URL = "https://github.com/manami-project/anime-offline-database/releases/latest/download/anime-offline-database.json"
 
 def download_database(app):
     app.log.info('Checking offline database')
     db_cache_path = fs.abspath('~/.config/anime_tools/db-cache')
     app.log.info('Checking in ' + db_cache_path)
     fs.ensure_dir_exists(db_cache_path)
-    offline_database = os.path.join(db_cache_path,"offline_database.json")
-    should_download_database: bool = False
-    if os.path.exists(offline_database):
-        age = time.time() - os.path.getmtime(offline_database)
-        if(age > 86400):
-            should_download_database = True
-    else:
-        should_download_database = True
+    offline_database = os.path.join(db_cache_path, "offline_database.json")
+
+    should_download_database = (
+        not os.path.exists(offline_database) or
+        (time.time() - os.path.getmtime(offline_database)) > 86400
+    )
+
     if should_download_database:
-        testfile = URLopener()
-        testfile.retrieve("https://github.com/manami-project/anime-offline-database/releases/download/latest/anime-offline-database.json", offline_database)
+        app.log.info('Downloading offline database...')
+        with urllib.request.urlopen(DB_URL) as resp, open(offline_database, 'wb') as out_f:
+            shutil.copyfileobj(resp, out_f)
         app.log.info('Offline database downloaded')
     else:
         app.log.info('Cached database is up to date')
